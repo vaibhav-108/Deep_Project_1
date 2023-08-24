56780c2efe1d8bda18e49ab9980fb4094e579ac8
vaibhav-108 <129012552+vaibhav-108@users.noreply.github.com>
2023-08-24 08:17:07 +0530

    Update app.py

--- app.py
+++ app.py
@@ -2,6 +2,7 @@ from flask import Flask,request,render_template
 import pandas as pd
 import numpy as np
 
+
 from sklearn.preprocessing import StandardScaler
 
 from src.pipeline.predict_pipeline import CustomData,PredictPipeline
